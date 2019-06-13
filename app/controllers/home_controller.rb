e9293820c393af1f5efcdcd20560bde3b6935250
kazuki-kitaguchi <higeohogehoge@gmail.com>
2019-06-14 08:48:18 +0900

    カラー名による検索機能を実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,23 @@
 class HomeController < ApplicationController
   def index
+    @search = Color.ransack(params[:q])
     @colors = Color.page(params[:page])
     view
   end
 
   def search
+    @search = Color.ransack(params[:q])
+    @colors = @search.result.page(params[:page])
+    preview
   end
   
   private
-  def view
-        @left_chart = params[:param_l]
-        @right_chart = params[:param_r]
+    def view
+      @left_chart = params[:param_l]
+      @right_chart = params[:param_r]
+    end
+    def preview
+      @left_chart = params[:q][:param_l]
+      @right_chart = params[:q][:param_r]
     end
 end
