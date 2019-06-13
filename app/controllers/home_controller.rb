9c2a400dba92b4370c542219baad21470c5e454c
kazuki-kitaguchi <higeohogehoge@gmail.com>
2019-06-14 08:10:05 +0900

    カラーリストから左右を選択して比較部に表示できるように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ApplicationController
   def index
     @colors = Color.page(params[:page])
+    view
   end
 
   def search
   end
+  
+  private
+  def view
+        @left_chart = params[:param_l]
+        @right_chart = params[:param_r]
+    end
 end
