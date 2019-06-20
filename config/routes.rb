a122b57e7f67936741a86d445f9131775b2b531a
kazuki-kitaguchi <higeohogehoge@gmail.com>
2019-06-20 16:23:38 +0900

    コメントアウトを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :sessions      => "users/sessions",
-    :registrations => "users/registrations",
-    :passwords     => "users/passwords"
-  }
+  devise_for :users
   resources :users, :only => [:index, :show]
   resources :makers
   resources :colors
