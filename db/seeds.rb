d221b861565afa7006dacf9d2ceebc30de26867c
kazuki-kitaguchi <higeohogehoge@gmail.com>
2019-10-30 21:04:52 +0900

    seedsファイルに先入れデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create( email: 'test@gmail.com', password: 'plamotone', admin: false )
+User.create( email: 'higeohogehoge@gmail.com', password: 'zetaz006', admin: true )
