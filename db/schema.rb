582261d7ec93c4af43b7ef4623f11da927e33f19
kazuki-kitaguchi <higeohogehoge@gmail.com>
2019-12-05 21:53:57 +0900

    userにguestカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_132722) do
+ActiveRecord::Schema.define(version: 2019_11_29_232846) do
 
   create_table "colors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "color_name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_10_03_132722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.boolean "guest", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
